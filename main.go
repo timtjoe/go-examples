e3e7425916dd9d50e0e0a722c513b0ec72d8eec0
timtjoe <ttjoe0@gmail.com>
2026-07-16 07:45:32 +0000

    add code from STC

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
+import ("bufio"; "fmt"; "os"; "strconv")
 
-import (
-	"fmt"
-	intermediate "go-examples/intermediate"
-)
+// type Shape interface { ... }
+// type Circle struct { ... }
+// func (c Circle) Area() float64 { ... }
 
 func main() {
-fmt.Println("Hello, World!")
-intermediate.RunChan()
+    sc := bufio.NewScanner(os.Stdin)
+    sc.Scan(); kind := sc.Text()
+    sc.Scan(); dim, _ := strconv.ParseFloat(sc.Text(), 64)
+    var s interface{ Area() float64 }
+    _ = kind; _ = dim
+    // s = ... based on kind
+    if s != nil { fmt.Printf("%.2f\n", s.Area()) }
 }
